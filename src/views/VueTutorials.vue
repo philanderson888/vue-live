d728a7c2561c5771ca5b0b3a3012f8977c123105
philanderson888 <philanderson888@hotmail.com>
2022-01-25 03:42:05 +0000

    add spaces

--- src/views/VueTutorials.vue
+++ src/views/VueTutorials.vue
@@ -4,9 +4,9 @@
       <div class="container">
         <h1 class="title">Vue Tutorials</h1>
         <ul>
-          <li>Vue Notes<a href="https://github.com/philanderson888/vue">https://github.com/philanderson888/vue</a></li>
-          <li>Vue Single Files For Learning<a href="https://github.com/philanderson888/vue/tree/master/single-files">https://github.com/philanderson888/vue/tree/master/single-files</a></li>
-          <li>Vue Projects<a href="https://github.com/philanderson888/vue/tree/master/projects">https://github.com/philanderson888/vue/tree/master/projects</a></li>
+          <li>Vue Notes <a href="https://github.com/philanderson888/vue">https://github.com/philanderson888/vue</a></li>
+          <li>Vue Single Files For Learning <a href="https://github.com/philanderson888/vue/tree/master/single-files">https://github.com/philanderson888/vue/tree/master/single-files</a></li>
+          <li>Vue Projects <a href="https://github.com/philanderson888/vue/tree/master/projects">https://github.com/philanderson888/vue/tree/master/projects</a></li>
         </ul>
       </div>
     </section>
