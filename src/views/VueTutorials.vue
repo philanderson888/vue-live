65f154be94f3a3e697ab4e7603394a897be1e294
philanderson888 <philanderson888@hotmail.com>
2022-01-25 03:40:49 +0000

    update links to vue learning

--- src/views/VueTutorials.vue
+++ src/views/VueTutorials.vue
@@ -4,8 +4,9 @@
       <div class="container">
         <h1 class="title">Vue Tutorials</h1>
         <ul>
-          <li>Single File Vue Tutorials <a href="https://github.com/philanderson888/vue/tree/master/single-files">https://github.com/philanderson888/vue/tree/master/single-files</a></li>
-          <li>Vue Tutorials both Standalone and complete @vue/cli projects <a href="https://github.com/philanderson888/vue">https://github.com/philanderson888/vue</a></li>
+          <li>Vue Notes<a href="https://github.com/philanderson888/vue">https://github.com/philanderson888/vue</a></li>
+          <li>Vue Single Files For Learning<a href="https://github.com/philanderson888/vue/tree/master/single-files">https://github.com/philanderson888/vue/tree/master/single-files</a></li>
+          <li>Vue Projects<a href="https://github.com/philanderson888/vue/tree/master/projects">https://github.com/philanderson888/vue/tree/master/projects</a></li>
         </ul>
       </div>
     </section>
