f7f38e4330d89b8d33355eed0dbbd1fcde7cff64
philanderson888 <philanderson888@hotmail.com>
2022-08-06 17:55:30 +0100

    reduce menu size

--- src/App.vue
+++ src/App.vue
@@ -42,19 +42,15 @@
               <a class="navbar-item" href="https://nervous-pare-b73c83.netlify.app/blog">Testimony App (built in NuxtJS)</a>
               <a class="navbar-item" href="https://jesusislord.uk">Jesus Is Lord Shopping Site (built by my daughter aged 11)</a>
               <hr class="navbar-divider" />
-             <a class="navbar-item">VueJS</a>
               <a class="navbar-item" href="https://epic-hamilton-f35937.netlify.app/">VueJS with Bulma CSS (This site)</a>
               <hr class="navbar-divider" />
               <a class="navbar-item">Node</a>
               <a class="navbar-item" href="https://strong-lebkuchen-5f4a43.netlify.app">Tailwind CSS</a>
               <hr class="navbar-divider" />
-              <a class="navbar-item">Contentful Blog Site</a>
               <a class="navbar-item" href="https://philanderson.uk">PhilAnderson.uk Contentful Static Site (need to fix build)</a>
               <hr class="navbar-divider" />
-              <a class="navbar-item">Deno</a>
               <a class="navbar-item" href="https://philanderson888-deno.deno.dev">Deno Demo Site</a>
               <hr class="navbar-divider" />
-              <a class="navbar-item">Svelte</a>
               <a class="navbar-item" href="https://svelte-hello-world-01.netlify.app/">Svelte Demo Site</a>
               <hr class="navbar-divider" />
               <a class="navbar-item" href="https://github.com/philanderson888/course-notes">Course Notes</a>
