8ef9d55d472f362cd8f6d18756d3926b75bda9b7
philanderson888 <philanderson888@hotmail.com>
2022-12-30 06:22:20 +0000

    remove archive files

--- archive/2022/12/first-version/src/views/Home2View.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TheWelcome2 from "../components/TheWelcome2.vue";
-</script>
-
-<template>
-  <main>
-    <TheWelcome2 />
-  </main>
-</template>
--- archive/2022/12/first-version/src/views/Home3View.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TheWelcome3 from "../components/TheWelcome3.vue";
-</script>
-
-<template>
-  <main>
-    <TheWelcome3 />
-  </main>
-</template>
